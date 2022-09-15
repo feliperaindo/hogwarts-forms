71f2a1fccb7965a86f681ce43ae1366d8e8f64d7
diegojsans <diego.jsantos@outlook.com>
2022-09-15 10:57:21 -0300

    Criando função para solução de validação 3°R

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputEmail = document.getElementById('validation-one');
+const inputSenha = document.getElementById('validation-two');
+const btn = document.getElementById('button-one');
+
+function inputAlert() {
+  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
+btn.addEventListener('click', inputAlert);
