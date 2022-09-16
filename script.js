f986afe38d949fea273f727baba8dc0935950367
feliperaindo <felipe.raindo@gmail.com>
2022-09-16 00:36:14 -0300

    resolve questão 18

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputEmail = document.getElementById('validation-one');
 const inputSenha = document.getElementById('validation-two');
 const btn = document.getElementById('button-one');
+const checkBox = document.getElementById('agreement');
 
 function inputAlert() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -9,4 +10,20 @@ function inputAlert() {
     alert('Email ou senha inválidos.');
   }
 }
+
+function activeButton() {
+  const btnFinal = document.getElementById('submit-btn');
+
+  if (checkBox.checked === true) {
+    btnFinal.removeAttribute('disabled', '');
+    btnFinal.classList.remove('btn-secondary');
+    btnFinal.classList.add('btn-primary');
+  } else {
+    btnFinal.setAttribute('disabled', '');
+    btnFinal.classList.remove('btn-primary');
+    btnFinal.classList.add('btn-secondary');
+  }
+}
+
 btn.addEventListener('click', inputAlert);
+checkBox.addEventListener('change', activeButton);
